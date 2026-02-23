a409ac7700163c2434ad8aefc0040a13e7996c56
piyush-0077 <khandharpiyush5@gmail.com>
2026-02-23 23:51:18 +0530

    javascript

--- prototype.js
+++ prototype.js
@@ -38,7 +38,7 @@ SuperHero.prototype.fightCrime = function () {
     console.log('Fighting crime')
 }
 
-//piiyush
+
 
 SuperHero.prototype = Object.create(person.prototype)
 const batman = new SuperHero('piyush' , 'khandhar')
